23add83efefc4c606b6830fd7c994d0b743a5bd1
guiogigo <guilhermevbatista20@gmail.com>
2023-04-04 20:36:12 -0300

    Sprint 3

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function menuToggle() {
   btnClose.classList.toggle('hide')
   btnOpen.classList.toggle('hide')
 }
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  hiddenClass: 'swiper-button-hidden',
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  //paginação
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
